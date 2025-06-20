331860713596becd393d88c4f0be988c091895df
liliamey <liliameyn985@gmail.com>
2025-06-20 07:33:16 +0700

    Add files via upload

---
+++ Media Persamaan Garis Lurus - Storyline output/story_content/user.js
@@ -0,0 +1,59 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ck1HWYlplS":
+        Script1();
+        break;
+      case "6MTgTpSts9E":
+        Script2();
+        break;
+      case "6LXVhQF3Kvl":
+        Script3();
+        break;
+      case "5rKOBPNsJcb":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="backsound.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.03;
+
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="backsound.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="backsound.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="backsound.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+
+}
+
